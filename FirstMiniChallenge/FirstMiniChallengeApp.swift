bbf8529d74489caf8e5cbd83a877a80851708c72
André Arns <andreluisarns@gmail.com>
2021-06-22 15:28:38 -0300

    Initial Commit

---
+++ FirstMiniChallenge/FirstMiniChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstMiniChallengeApp.swift
+//  FirstMiniChallenge
+//
+//  Created by André Arns on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstMiniChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
