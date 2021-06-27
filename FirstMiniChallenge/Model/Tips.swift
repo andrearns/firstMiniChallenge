5c5ebf694db4bb0b7c52ea2d337ee911b3660518
DaneOnishi <daniellaonishi07@gmail.com>
2021-06-27 00:37:14 -0300

    Adds updates on TipsView, tips cards design and tips struct

---
+++ FirstMiniChallenge/Model/Tips.swift
@@ -0,0 +1,16 @@
+//
+//  Tips.swift
+//  FirstMiniChallenge
+//
+//  Created by Daniella Onishi on 26/06/21.
+//
+
+import Foundation
+
+
+struct Tips {
+    var id = UUID()
+    var title: String
+    var bodyText: String
+    var color: String
+}
